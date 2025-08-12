4d64a3eaf571bd507f04f709bc1cf95754efbdc2
bipeen kumar <bipeen@gmail.com>
2025-08-13 01:18:21 +0530

    invoke cloudfront

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,10 +1,11 @@
-output "role_arn" {
-  description = "IAM Role ARN for Lambda@Edge execution"
-  value       = aws_iam_role.lambda_edge_execution.arn
+output "wordpress_instance_profile_name" {
+  value = aws_iam_instance_profile.wordpress_instance_profile.name
 }
 
-output "wordpress_instance_profile_name" {
-  description = "Name of the IAM instance profile for WordPress EC2 instances"
-  value       = aws_iam_instance_profile.wordpress_instance_profile.name
+output "lambda_edge_execution_role_arn" {
+  value = aws_iam_role.lambda_edge_execution.arn
+}
 
+output "lambda_edge_execution_role_name" {
+  value = aws_iam_role.lambda_edge_execution.name
 }
